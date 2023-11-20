3119628f0abd8ef02ca29dae96dfc0696bf5eb50
ember97 <emmieberger@live.se>
2023-11-20 15:45:29 +0100

    Initial movement of sphere around a cube

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,54 @@
+using UnityEditor.PackageManager;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Example : MonoBehaviour
+{
+    public GameObject target;
+
+    private float time = 0.0f;
+    public float directionChangeTime = 30f;
+    private Vector3 directionVector;
+    private float degrees;
+
+    void Update()
+    {
+        // Rotate target
+        transform.RotateAround(target.transform.position, directionVector, degrees * Time.deltaTime);
+
+        // Increase time 
+        time += Time.deltaTime;
+
+        // Change direction if it is time
+        if (time >= directionChangeTime)
+        {
+            time = time - directionChangeTime;
+            directionVector = genDirectionVector();
+            
+        }
+    }
+
+
+    void Start()
+    {
+        directionVector = genDirectionVector();
+        degrees = 20;
+    }
+
+    
+
+    Vector3 genDirectionVector() {
+        int x = Random.Range(0, 4);
+        switch (x) { 
+            case 0:
+                return -Vector3.forward;
+            case 1:
+                return Vector3.forward;
+            case 2:
+                return Vector3.up;
+            default:
+                return Vector3.down;
+        }
+    
+    }
+}
