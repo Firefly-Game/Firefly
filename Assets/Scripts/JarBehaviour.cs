c2520b085b2b31f784c6377fc0d26d894a7e1134
krystynaziobro <48292636+krystynaziobro@users.noreply.github.com>
2023-11-23 21:05:20 +0100

    Adding collision

---
+++ Assets/Scripts/JarBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class JarBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Something has entered the jar");
+        if (other.gameObject.CompareTag("fly"))
+        {
+            Debug.Log("A firefly has entered the jar");
+            // Deactivate the firefly
+            other.gameObject.SetActive(false);
+        }
+    }
+}
